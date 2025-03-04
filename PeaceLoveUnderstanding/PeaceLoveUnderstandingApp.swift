5cf778887857e34e13ab425398faf90f8b8042d4
Gabe Witcher <gabewitcher@gmail.com>
2025-03-04 14:15:44 -0800

    Initial Commit

---
+++ PeaceLoveUnderstanding/PeaceLoveUnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeaceLoveUnderstandingApp.swift
+//  PeaceLoveUnderstanding
+//
+//  Created by Gabe Witcher on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PeaceLoveUnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

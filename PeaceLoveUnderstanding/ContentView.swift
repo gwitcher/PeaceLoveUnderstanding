61a373126866626acf128f758e23a589788cb02d
Gabe Witcher <gabewitcher@gmail.com>
2025-03-04 15:10:15 -0800

    Elvis App

--- PeaceLoveUnderstanding/ContentView.swift
+++ PeaceLoveUnderstanding/ContentView.swift
@@ -8,12 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(Color.purple)
+            
+            Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .frame(width: 300, height: 300)
+               
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(Color.purple)
+                
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    imageName = "peacesign"
+                }
+                Button("Love") {
+                    message = "Love"
+                    imageName = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    imageName = "lightbulb"
+                }
+               
+            }
+            .buttonStyle(.borderedProminent)
+            //.font(.title2)
+            //.fontWeight(.bold)
+            .tint(.purple)
         }
         .padding()
     }
